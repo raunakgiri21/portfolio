6da12202f7ce7ed4bb1eef0716e0830d6cbf8c85
Raunak <raunakgiri21@gmail.com>
2023-09-28 13:59:25 +0530

    Projects Data Changed

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -25,33 +25,33 @@ const Projects = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
         <ProjectCard
           title="FOODSTRAP e-Commerce"
-          des="lorem, ipsum dolor sit smasdkn fiir3f3 iurfnio3f iornfoi3nrpowef oiuhrfoinerf oiunfr3"
+          des="MERN Stack Project with google signin/signup, product filter/search/sort, wishlist, add-to-cart, razorpay payment gateway."
           src={foodstrap}
         />
         <ProjectCard
           title="Mini Twitter Clone"
-          des="lorem, ipsum dolor sit smasdkn fiir3f3 iurfnio3f iornfoi3nrpowef oiuhrfoinerf oiunfr3"
+          des="MERN Stack mini project with signin/signup, create/like/delete a tweet, follow-unfollow, etc."
           src={twitter}
         />
         <ProjectCard
           title="Netflix Clone"
-          des="lorem, ipsum dolor sit smasdkn fiir3f3 iurfnio3f iornfoi3nrpowef oiuhrfoinerf oiunfr3"
+          des="UI clone of Netflix built with ReactJS, other features are signin/signup/logout, using TMDB API."
           src={netflix}
         />
         <ProjectCard
-          title="FOODSTRAP e-Commerce"
-          des="lorem, ipsum dolor sit smasdkn fiir3f3 iurfnio3f iornfoi3nrpowef oiuhrfoinerf oiunfr3"
-          src={foodstrap}
+          title="NIKE UI Clone"
+          des="A simple static website built using ReactJs and TailwindCSS for  seamless user experiences."
+          src={nike}
         />
         <ProjectCard
-          title="Mini Twitter Clone"
-          des="lorem, ipsum dolor sit smasdkn fiir3f3 iurfnio3f iornfoi3nrpowef oiuhrfoinerf oiunfr3"
-          src={twitter}
+          title="AI Summary"
+          des="Built a website with ReactJS and TailwindCSS which gives us a summary of any news article using AI based Rapid-API."
+          src={aisumz}
         />
         <ProjectCard
-          title="Netflix Clone"
-          des="lorem, ipsum dolor sit smasdkn fiir3f3 iurfnio3f iornfoi3nrpowef oiuhrfoinerf oiunfr3"
-          src={netflix}
+          title="Weather.ly"
+          des="Built a weather forecasting website using HTML, CSS, Javascript and openweathermap-api."
+          src={weather}
         />
       </div>
     </section>
